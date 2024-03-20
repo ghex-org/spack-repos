97550777cb1ab2cc95840d70be67e61372f41b96
boeschf <48126478+boeschf@users.noreply.github.com>
2024-03-20 18:33:48 +0100

    add ghex package and ci workflow

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
 [![Test: hwmalloc](https://github.com/ghex-org/spack-repos/actions/workflows/hwmalloc.yml/badge.svg)](https://github.com/ghex-org/spack-repos/actions/workflows/hwmalloc.yml)
 [![Test: oomph](https://github.com/ghex-org/spack-repos/actions/workflows/oomph.yml/badge.svg)](https://github.com/ghex-org/spack-repos/actions/workflows/oomph.yml)
+[![Test: ghex](https://github.com/ghex-org/spack-repos/actions/workflows/ghex.yml/badge.svg)](https://github.com/ghex-org/spack-repos/actions/workflows/ghex.yml)
 # spack-repos
 Spack packages for ghex-org repositories
---
+++ packages/ghex/package.py
@@ -0,0 +1,66 @@
+from spack.package import *
+
+class Ghex(CMakePackage, CudaPackage, ROCmPackage):
+    """"GHEX is a generic halo-exchange library."""
+
+    homepage="https://github.com/ghex-org/GHEX"
+    url = "https://github.com/ghex-org/GHEX/archive/refs/tags/v0.3.0.tar.gz"
+    
+    git = "https://github.com/ghex-org/GHEX.git"
+    maintainers = ["boeschf"]
+
+    version("0.4.0", sha256="a549c3d9d033c1bf7ee3a6f283cdd086192fd02eea0489eb8728df5acfc56564")
+    version("0.3.0", sha256="2629b40ac947817886cc5f06bc594abd47352e09cd756a358db35c03c7579a1c")
+    # for dev-build
+    version("develop")
+
+    variant("xpmem", default=False, description="Use xpmem shared memory")
+    variant("python", default=True, description="Build Python bindings")
+
+    depends_on("cmake@3.21:")
+    depends_on("mpi")
+    depends_on("boost")
+    depends_on("googletest", type=("build", "test"))
+
+    depends_on("oomph")
+    depends_on("oomph+cuda", when="+cuda")
+    depends_on("oomph+rocm", when="+rocm")
+    depends_on("oomph@0.3:", when="@0.3:")
+
+    extends("python", when="+python")
+    depends_on("python@3.7:", when="+python", type="build")
+    depends_on("py-pip", when="+python", type="build")
+    depends_on("py-pybind11", when="+python", type="build")
+    depends_on("py-mpi4py", when="+python", type=("build", "run"))
+    depends_on("py-numpy", when="+python", type=("build", "run"))
+    depends_on("py-pytest", when="+python", type=("test"))
+
+    def cmake_args(self):
+        spec = self.spec
+
+        if spec["oomph"].satisfies("backend=ucx", False):
+            backend = "UCX"
+        elif spec["oomph"].satisfies("backend=libfabric", False):
+            backend = "LIBFABRIC"
+        else:
+            backend = "MPI"
+
+        pyexe = spec["python"].command.path
+
+        args = [
+            self.define("GHEX_USE_BUNDLED_LIBS", True),
+            self.define("GHEX_USE_BUNDLED_GRIDTOOLS", True),
+            self.define("GHEX_USE_BUNDLED_OOMPH", False),
+            self.define("GHEX_USE_BUNDLED_GTEST", False),
+            self.define("GHEX_TRANSPORT_BACKEND", backend),
+            self.define_from_variant("GHEX_USE_XPMEM", "xpmem"),
+            self.define_from_variant("GHEX_BUILD_PYTHON_BINDINGS", "python"),
+            self.define("GHEX_PYTHON_LIB_PATH", python_platlib),
+            self.define("GHEX_WITH_TESTING", self.run_tests),
+        ]
+
+        if self.run_tests:
+            args.append("-DMPIEXEC_PREFLAGS=--oversubscribe")
+
+        return args
+        
